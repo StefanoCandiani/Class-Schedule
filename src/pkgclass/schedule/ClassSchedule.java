1aa8726ee3c7a661294e1648763229fd39807e2f
Stefano Candiani <22500@columbushs.com>
2019-09-04 14:06:09 -0400

    Done!

---
+++ src/pkgclass/schedule/ClassSchedule.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule;
+
+/**
+ *
+ * @author 22500 (Stefano Candiani)
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nameStudent = "Stefano Candiani";
+        int graduationYear = 2022;
+        String course1 = "Homeroom";
+        String teacher1 = "Ms. Bell-Mendoza";
+        String course2 = "Personal & Family Finance";
+        String teacher2 = "Mr. O'Neil";
+        String course3 = "Algebra 2 Accelerated";
+        String teacher3 = "Ms. Acevedo";
+        String course4 = "Chemistry Accelerated";
+        String teacher4 = "Ms. Reynaldo";
+        String course5 = "Honors English";
+        String teacher5 = "Ms. Mallafre";
+        String course6 = "French 2";
+        String teacher6 = "Ms. Vessiere";
+        String course7 = "Christology";
+        String teacher7 = "Mr. Machado";
+        String course8 = "Honors Computer Science";
+        String teacher8 = "Mr. Larrubia";
+        
+        System.out.println("+------------------------------------------------------------+" );
+        System.out.println("| Name|" +nameStudent+ "                                      | ");
+        System.out.println("| Grad|" +graduationYear+ "                                                  | ");
+        System.out.println("|------------------------------------------------------------|" );
+        System.out.println("| P HR|" +course1+ "                                              |" );
+        System.out.println("|     |" +teacher1+ "                                      |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 1 |" +course2+ "                             |" );
+        System.out.println("|     |" +teacher2+ "                                            |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 2 |" +course3+"                                 |" );
+        System.out.println("|     |" +teacher3+ "                                           |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 3 |" +course4+"                                 |" );
+        System.out.println("|     |" +teacher4+"                                          |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 4 |" +course5+"                                        |" );
+        System.out.println("|     |" +teacher5+"                                          |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 5 |" +course6+"                                              |" );
+        System.out.println("|     |" +teacher6+"                                          |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 6 |" +course7+"                                           |" );
+        System.out.println("|     |" +teacher7+"                                           |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("| P 7 |" +course8+"                               |" );
+        System.out.println("|     |" +teacher8+"                                          |" );
+        System.out.println("|     |                                                      |" );
+        System.out.println("+------------------------------------------------------------+" );
+        // TODO code application logic here
+    }
+    
+}
